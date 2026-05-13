f2fdc881f3f80103f850094bb6db815b73203b33
maaeda <133735669+maaeda@users.noreply.github.com>
2026-05-13 15:18:45 +0900

    Initialize Firebase in main function

--- chat_application/lib/main.dart
+++ chat_application/lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'screens/chat_list_screen.dart';
 import 'screens/settings_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
